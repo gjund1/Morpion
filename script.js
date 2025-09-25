cff2a00d2e340866656c146d54667c445e98f6f7
JUND Guy <gjund@free.fr>
2025-09-25 09:49:09 +0200

    Morpion 1.2

---
+++ script.js
@@ -0,0 +1,36 @@
+const game = document.querySelector("#game")
+
+const board = [
+    [0, 1, 0],
+    [0, 0, 0],
+    [2, 0, 0],
+]
+
+let playeur = 1
+
+function cleanBoard () {
+    while (game.firstChild)
+        game.removeChild(game.firstChild)
+}
+
+function generatBoard (board) {
+    cleanBoard(board)
+
+    board.forEach((line, lineIndex) => {
+        // line = [0, 1, 0]  lineIndex = 0, 1 ou 2e ligne
+        const lineDiv = document.createElement("div")
+        lineDiv.classList.add("line")
+        game.appendChild(lineDiv)
+
+        line.forEach((value, squareIndex) => {
+            // value = 0 vide, playeur 1, playeur 2
+            // squareIndex = case 1, 2 ou 3
+            const square = document.createElement("div")
+            square.classList.add("square")
+            square.dataset.state = value
+            lineDiv.appendChild(square)
+        })
+    })
+}
+
+generatBoard (board)
